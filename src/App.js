ba5b75335ef4e0c432728431adebda9c7f7bf301
ikky <mrizqimaulana1@gmail.com>
2026-09-11 01:11:30 +0700

    Tarik posisi foto ke bawah agar pas di tengah kotak putih

--- src/App.js
+++ src/App.js
@@ -242,36 +242,36 @@ function App() {
               overflow: 'hidden'
             }}
           >
-        {/* TAMPILAN 6 FOTO */}
+{/* TAMPILAN 6 FOTO */}
             {photos.length > 0 && (
               <div style={{ position: 'absolute', top: '0px', left: '0px', width: '100%', height: '100%', zIndex: 1 }}>
                 
                 {/* Baris 1 (Foto 1 & 4) */}
-                <div style={{ position: 'absolute', top: '75px', left: '46px', width: '308px', height: '105px', display: 'flex', justifyContent: 'space-between' }}>
-                  <div style={{ width: '135px', height: '105px', overflow: 'hidden' }}>
+                <div style={{ position: 'absolute', top: '95px', left: '46px', width: '308px', height: '115px', display: 'flex', justifyContent: 'space-between' }}>
+                  <div style={{ width: '135px', height: '115px', overflow: 'hidden' }}>
                     <img src={photos[0]} alt="L-0" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }} />
                   </div>
-                  <div style={{ width: '135px', height: '105px', overflow: 'hidden' }}>
+                  <div style={{ width: '135px', height: '115px', overflow: 'hidden' }}>
                     <img src={photos[3]} alt="R-0" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }} />
                   </div>
                 </div>
 
                 {/* Baris 2 (Foto 2 & 5) */}
-                <div style={{ position: 'absolute', top: '235px', left: '46px', width: '308px', height: '105px', display: 'flex', justifyContent: 'space-between' }}>
-                  <div style={{ width: '135px', height: '105px', overflow: 'hidden' }}>
+                <div style={{ position: 'absolute', top: '260px', left: '46px', width: '308px', height: '115px', display: 'flex', justifyContent: 'space-between' }}>
+                  <div style={{ width: '135px', height: '115px', overflow: 'hidden' }}>
                     <img src={photos[1]} alt="L-1" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }} />
                   </div>
-                  <div style={{ width: '135px', height: '105px', overflow: 'hidden' }}>
+                  <div style={{ width: '135px', height: '115px', overflow: 'hidden' }}>
                     <img src={photos[4]} alt="R-1" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }} />
                   </div>
                 </div>
 
                 {/* Baris 3 (Foto 3 & 6) */}
-                <div style={{ position: 'absolute', top: '395px', left: '46px', width: '308px', height: '105px', display: 'flex', justifyContent: 'space-between' }}>
-                  <div style={{ width: '135px', height: '105px', overflow: 'hidden' }}>
+                <div style={{ position: 'absolute', top: '425px', left: '46px', width: '308px', height: '115px', display: 'flex', justifyContent: 'space-between' }}>
+                  <div style={{ width: '135px', height: '115px', overflow: 'hidden' }}>
                     <img src={photos[2]} alt="L-2" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }} />
                   </div>
-                  <div style={{ width: '135px', height: '105px', overflow: 'hidden' }}>
+                  <div style={{ width: '135px', height: '115px', overflow: 'hidden' }}>
                     <img src={photos[5]} alt="R-2" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scaleX(-1)' }} />
                   </div>
                 </div>
