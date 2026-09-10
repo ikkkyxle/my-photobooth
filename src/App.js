079f3630c97b01d57078de350daf1bcad50c7782
ikky <mrizqimaulana1@gmail.com>
2026-09-11 00:36:59 +0700

    Koreksi posisi top foto ke atas agar pas di kotak pertama Frame 1

--- src/App.js
+++ src/App.js
@@ -242,12 +242,12 @@ function App() {
               overflow: 'hidden'
             }}
           >
- {/* TAMPILAN 6 FOTO */}
+          {/* TAMPILAN 6 FOTO */}
             {photos.length > 0 && (
               <div style={{ position: 'absolute', top: '0px', left: '0px', width: '100%', height: '100%', zIndex: 1 }}>
                 
                 {/* Kolom Kiri (Foto 1, 2, 3) */}
-                <div style={{ position: 'absolute', top: '228px', left: '76px', display: 'flex', flexDirection: 'column', gap: '58px' }}>
+                <div style={{ position: 'absolute', top: '78px', left: '76px', display: 'flex', flexDirection: 'column', gap: '58px' }}>
                   {photos.slice(0, 3).map((photo, index) => (
                     <div key={index} style={{ width: '118px', height: '105px', overflow: 'hidden' }}>
                       <img src={photo} alt={`L-${index}`} style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block', transform: 'scaleX(-1)' }} />
@@ -256,14 +256,13 @@ function App() {
                 </div>
 
                 {/* Kolom Kanan (Foto 4, 5, 6) */}
-                <div style={{ position: 'absolute', top: '228px', left: '226px', display: 'flex', flexDirection: 'column', gap: '58px' }}>
+                <div style={{ position: 'absolute', top: '78px', left: '226px', display: 'flex', flexDirection: 'column', gap: '58px' }}>
                   {photos.slice(3, 6).map((photo, index) => (
                     <div key={index} style={{ width: '118px', height: '105px', overflow: 'hidden' }}>
                       <img src={photo} alt={`R-${index}`} style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block', transform: 'scaleX(-1)' }} />
                     </div>
                   ))}
                 </div>
-
               </div>
             )}
 
