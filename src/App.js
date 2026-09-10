7999f6fb8dc817b9762edad5ee9c4dce053eed67
ikky <mrizqimaulana1@gmail.com>
2026-09-11 00:40:00 +0700

    Sesuaikan gap dan tinggi kotak foto Frame 1 agar pas sempurna

--- src/App.js
+++ src/App.js
@@ -242,27 +242,28 @@ function App() {
               overflow: 'hidden'
             }}
           >
-          {/* TAMPILAN 6 FOTO */}
+            {/* TAMPILAN 6 FOTO */}
             {photos.length > 0 && (
               <div style={{ position: 'absolute', top: '0px', left: '0px', width: '100%', height: '100%', zIndex: 1 }}>
                 
                 {/* Kolom Kiri (Foto 1, 2, 3) */}
-                <div style={{ position: 'absolute', top: '78px', left: '76px', display: 'flex', flexDirection: 'column', gap: '58px' }}>
+                <div style={{ position: 'absolute', top: '78px', left: '76px', display: 'flex', flexDirection: 'column', gap: '64px' }}>
                   {photos.slice(0, 3).map((photo, index) => (
-                    <div key={index} style={{ width: '118px', height: '105px', overflow: 'hidden' }}>
+                    <div key={index} style={{ width: '118px', height: '118px', overflow: 'hidden' }}>
                       <img src={photo} alt={`L-${index}`} style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block', transform: 'scaleX(-1)' }} />
                     </div>
                   ))}
                 </div>
 
                 {/* Kolom Kanan (Foto 4, 5, 6) */}
-                <div style={{ position: 'absolute', top: '78px', left: '226px', display: 'flex', flexDirection: 'column', gap: '58px' }}>
+                <div style={{ position: 'absolute', top: '78px', left: '226px', display: 'flex', flexDirection: 'column', gap: '64px' }}>
                   {photos.slice(3, 6).map((photo, index) => (
-                    <div key={index} style={{ width: '118px', height: '105px', overflow: 'hidden' }}>
+                    <div key={index} style={{ width: '118px', height: '118px', overflow: 'hidden' }}>
                       <img src={photo} alt={`R-${index}`} style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block', transform: 'scaleX(-1)' }} />
                     </div>
                   ))}
                 </div>
+
               </div>
             )}
 
